2234e2b0c8e6992cc3c1a8539b164fa95fa5bdb7
Robson Flavio Freitas <robson.flavio20@outlook.com>
2021-11-30 17:36:46 -0300

    Classe para montar a tabela nas views de Tabela_Campeonatos

---
+++ src/Controller/SimpleTableModel.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author PremierSoft
+ */
+public class SimpleTableModel extends AbstractTableModel {
+
+    private ArrayList linhas = null;
+    private String[] colunas = null;
+
+    public ArrayList getLinhas() {
+        return linhas;
+    }
+
+    public void setLinhas(ArrayList linhas) {
+        this.linhas = linhas;
+    }
+
+    public String[] getColunas() {
+        return colunas;
+    }
+
+    public void setColunas(String[] colunas) {
+        this.colunas = colunas;
+    }
+
+    @Override
+    public int getRowCount() {
+        return getLinhas().size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return getColunas().length;
+    }
+
+    @Override
+    public String getColumnName(int coluna) {
+        return colunas[coluna];
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        String[] linha = (String[]) getLinhas().get(rowIndex);
+        return linha[columnIndex];
+    }
+
+    public SimpleTableModel(ArrayList dados, String[] colunas) {
+        this.setColunas(colunas);
+        this.setLinhas(dados);
+    }
+
+}
